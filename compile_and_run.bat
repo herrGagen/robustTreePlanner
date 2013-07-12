699d05f963ec7e629f35b5c1581f23831c7c41f0
herrGagen <zuber139@gmail.com>
2013-07-12 16:45:56 -0400

    Changed default configuration to Release when building from the command line.

--- compile_and_run.bat
+++ compile_and_run.bat
@@ -1,7 +1,7 @@
 :: Compile using "MSBuild", then call the necessary commands to generate a tree.
 
 :: Compile
-MSBuild.exe RobustTree.sln
+MSBuild.exe /p:Configuration=Release RobustTree.sln
 
 :: Convert weather to a format useable by RTP, pass it all the command line parameters
 :: This also calls ``create_input.rb`` to create the "inputs.txt" file
