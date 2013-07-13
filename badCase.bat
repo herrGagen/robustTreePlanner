6c86f95209b4f7601bd287592ba811334aa9b571
herrGagen <zuber139@gmail.com>
2013-07-13 16:11:49 -0400

    Removes output files between runs to make it obvious when we break the generator.

--- badCase.bat
+++ badCase.bat
@@ -1,3 +1,6 @@
+del outout1.xml
+del Quadrant_4.xml
+
 CALL compile_and_run.bat -lanewidth 4 -angle 270 -s 0 -o 0 -dthresh 0.7 -nethresh 0.5 -oname outout1 -twname outout1 -cinput originalInput.txt -iname 0600 -angle 270 -quadrantsize 90 -cellwidth 2.761195 -operflex 1.6567 2.2089 2.7612 3.3134 3.8657 -fixnodes 1 -demandshift 1 -demanddrop 8
 
 RobustTree.exe -cinput Quadrant_4.txt
