b7a7e5291aff240837439848be7c0bc4e0870f0d
herrGagen <zuber139@gmail.com>
2013-07-13 12:10:58 -0400

    Runs 2 tests now: one with command line inputs and the other on what is a confirmed bad case from Joe.
    Also tests them using the ruby checker after the runs are complete.

--- badCase.bat
+++ badCase.bat
@@ -1,1 +1,9 @@
-.\compile_and_run.bat -s 0 -o 0 -dthresh 0.7 -nethresh 0.5 -oname outout1 -twname outout1 -cinput originalInput.txt -iname 0600a -angle 270 -quadrantsize 90 -cellwidth 2.761195 -operflex 1.6567 2.2089 2.7612 3.3134 3.8657 4.4179 4.9701 5.5224 6.6269 7.7313 8.8358 9.9403 11.0448 16.5671 -fixnodes 1 -demandshift 1 -demanddrop 8 -lanewidth 2
+CALL compile_and_run.bat -lanewidth 4 -angle 270 -s 0 -o 0 -dthresh 0.7 -nethresh 0.5 -oname outout1 -twname outout1 -cinput originalInput.txt -iname 0600 -angle 270 -quadrantsize 90 -cellwidth 2.761195 -operflex 1.6567 2.2089 2.7612 3.3134 3.8657 -fixnodes 1 -demandshift 1 -demanddrop 8
+
+RobustTree.exe -cinput Quadrant_4.txt
+
+echo Checking outout1.xml
+ruby bugCheck.rb outout1.xml
+
+echo checking Quadrant_4.xml
+ruby bugCheck.rb Quadrant_4.xml
