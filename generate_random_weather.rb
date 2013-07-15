e723e9300d6305728d531c266e1bb1730c5a34af
Will Hall <wrsh07@gmail.com>
2013-07-14 23:17:36 -0400

    Added a substitude file for convert_files.rb and made generate...rb output in the same way convert_files.rb ordinarily would.

--- generate_random_weather.rb
+++ generate_random_weather.rb
@@ -15,7 +15,7 @@ if __FILE__ == $0
   min_prob = 0.7
   altitudes = ["10000", "15000", "20000", "25000", "30000"]
   file_probabilities = { 1 => 0.8, 2 => 0.1, 3 => 0.02, 4 => 0.02, 5 => 0.02, 6 => 0.02 }
-  weather_dir = "RandomWeather"
+  weather_dir = "Data"
 
   ARGV.each_with_index do |arg, index|
     seed                = ARGV[index+1].to_i if arg == "-seed"
@@ -47,23 +47,18 @@ if __FILE__ == $0
     file_name = Pathname.new(seed_dir) + file
 
     File.open(file_name, 'w') do |f|
-      f.write("# Product type: CONVECTION\n")
-      f.write("# Variable: DevProb\n")
-      f.write("# Unit: scalar\n")
-      f.write("# Input filename: null\n")
-      f.write("# Member: 1/6\n")
-      f.write("# Probability: ")
+      f.write("Ensemble Member: ")
+      f.write(member_number.to_s)
+      f.write("/")
+      f.write(number_of_members.to_s + "\n")
+      f.write("Probability ")
       if member_number < number_of_members
         f.write(file_probabilities[member_number].to_s) 
         probability_written += file_probabilities[member_number]
       else if member_number == number_of_members
         f.write((1 - probability_written).to_s)
       end
-      f.write("\n# Reference time: 20090618T060000\n")
-      f.write("# Valid time, min valid time range, max valid time range: 20090618T060000,20090618T060000,20090618T061500\n")
-      f.write("# Grid size x,y,z: 322, 273, 5\n")
-      f.write("# Grid spacing (meters,meters,ft): 5000.00000,5000.00000,5000.00000\n")
-      f.write("# Dataset: x,y,z,lat,lon,alt,value\n")
+      f.write("\n")
       
       num_weather_points.times do
         # Generate latitude between 38 and 45.5
