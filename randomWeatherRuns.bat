c1049f9410cb424ae1da2add6e4035e81dff30ee
herrGagen <zuber139@gmail.com>
2013-07-15 11:10:14 -0400

    Seed loop was only over 5.

--- randomWeatherRuns.bat
+++ randomWeatherRuns.bat
@@ -9,7 +9,7 @@ set MAX_SEED=10000
 set xmlName=blank
 
 FOR /L %%n IN (%MIN_NUMEL%,100,%MAX_NUMEL%) DO @(
-  FOR /L %%s IN (1,1,5) DO @(
+  FOR /L %%s IN (%MIN_SEED%,1,%MAX_SEED%) DO @(
     FOR /L %%a IN (0, 90, 270) DO @(
       FOR %%w IN (-1 2 4) DO @(
         set xmlName=seed%%s_angle%%a_laneWidth%%w_numPoints%%n_numMembers%NUM_MEMBERS%
