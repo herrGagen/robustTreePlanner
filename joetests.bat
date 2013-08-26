3bab60dddbc2e5f8e11253ba703af993bacbecf2
Will Hall <wrsh07@gmail.com>
2013-08-26 01:36:40 -0400

    Included angle variation in our batch runs.

--- joetests.bat
+++ joetests.bat
@@ -14,7 +14,7 @@ echo Starting
 :: offset = 120
 :: date = all days
 :: start time = first starting time per weather set
-for %%n in (1 2 4 8 9) do @(
+for %%n in (1 2 8 9 10) do @(
 	for /l %%o in (120,30,120) do @(
 		for %%d in (20090424 20090427 20090618 20110722) do @(
       echo Third %%d
@@ -26,10 +26,10 @@ for %%n in (1 2 4 8 9) do @(
 					set firstTime=0
 					echo set weatherDay=Weather\%%d\%%t
 					echo !weatherDay!
-					for /l %%a in (0,90,270) do @(
+					for /l %%a in (0,90,180,270) do @(
 						set outputName=XML/neThresh_0p%%n_weatherTime_%%t_weatherDay_%%d_offset_%%o_angle_%%a
 						if not exist !outputName!.xml @(							
-							start /b cmd /c call run.bat -nethresh 0.%%n -iname !weatherDay! -o %%o -oname !outputName! -cellwidth 1 -lanewidth 3 -s 0 -twname tempWeather -cinput !outputName!.joetests.txt -quadrantsize 90 -operflex 1.0 2.0 4.0 6 8 10 12 14 16 18 20 22 24 -fixnodes 1 -demandshift 1 -demanddrop 4 -dthresh 0.8 -concat
+							start /b cmd /c call run.bat -angle %%a -nethresh 0.%%n -iname !weatherDay! -o %%o -oname !outputName! -cellwidth 1 -lanewidth 3 -s 0 -twname tempWeather -cinput !outputName!.joetests.txt -quadrantsize 90 -operflex 1.0 2.0 4.0 6 8 10 12 14 16 18 20 22 24 -fixnodes 1 -demandshift 1 -demanddrop 4 -dthresh 0.8 -concat
 						)
 					)
 				)
@@ -39,11 +39,11 @@ for %%n in (1 2 4 8 9) do @(
 )
 
 :: Next, only vary duration and day
-:: edge threshold = .1
+:: edge threshold = .9
 :: offset = 0 30 60 90 120
 :: date = all days
 :: start time = first starting time per weather set
-for %%n in (1) do @(
+for %%n in (9) do @(
 	for /l %%o in (0,30,120) do @(
 		for %%d in (20090424 20090427 20090618 20110722) do @(
 			set dirname=%cd%\Weather\%%d\*
@@ -53,10 +53,10 @@ for %%n in (1) do @(
 					set firstTime=0
 					set weatherDay=Weather\%%d\%%t
 					echo !weatherDay!
-					for /l %%a in (0,90,270) do @(
+					for /l %%a in (0,90,180,270) do @(
 						set outputName=XML/neThresh_0p%%n_weatherTime_%%t_weatherDay_%%d_offset_%%o_angle_%%a
 						if not exist !outputName!.xml @(
-							start /b cmd /c call run.bat -nethresh 0.%%n -iname !weatherDay! -o %%o -oname !outputName! -cellwidth 1 -lanewidth 3 -s 0 -twname tempWeather -cinput !outputName!.joetests.txt -quadrantsize 90 -operflex 1.0 2.0 4.0 6 8 10 12 14 16 18 20 22 24 -fixnodes 1 -demandshift 1 -demanddrop 4 -dthresh 0.8 -concat
+							start /b cmd /c call run.bat -angle %%a -nethresh 0.%%n -iname !weatherDay! -o %%o -oname !outputName! -cellwidth 1 -lanewidth 3 -s 0 -twname tempWeather -cinput !outputName!.joetests.txt -quadrantsize 90 -operflex 1.0 2.0 4.0 6 8 10 12 14 16 18 20 22 24 -fixnodes 1 -demandshift 1 -demanddrop 4 -dthresh 0.8 -concat
 						)
 					)
 				)			
@@ -66,21 +66,21 @@ for %%n in (1) do @(
 )
 
 :: Finally loop over all weather combinations
-:: edge threshold = .1 
+:: edge threshold = .9 
 :: offset = 120
 :: date = all days
 :: start time = all starting times
-for %%n in (1) do @(
+for %%n in (9) do @(
 	for /l %%o in (120,30,120) do @(
 		for %%d in (20090424 20090427 20090618 20110722) do @(
 			set dirname=%cd%\Weather\%%d\*
 			for /f %%t in ('dir /d /b !dirname!') do @(
 				set weatherDay=Weather\%%d\%%t
 				echo !weatherDay!
-				for /l %%a in (0,90,270) do @(
+				for /l %%a in (0,90,180,270) do @(
 					set outputName=XML/neThresh_0p%%n_weatherTime_%%t_weatherDay_%%d_offset_%%o_angle_%%a
 					if not exist !outputName!.xml @(
-						start /b cmd /c call run.bat -nethresh 0.%%n -iname !weatherDay! -o %%o -oname !outputName! -cellwidth 1 -lanewidth 3 -s 0 -twname tempWeather -cinput !outputName!.joetests.txt -quadrantsize 90 -operflex 1.0 2.0 4.0 6 8 10 12 14 16 18 20 22 24 -fixnodes 1 -demandshift 1 -demanddrop 4 -dthresh 0.8 -concat
+						start /b cmd /c call run.bat -angle %%a -nethresh 0.%%n -iname !weatherDay! -o %%o -oname !outputName! -cellwidth 1 -lanewidth 3 -s 0 -twname tempWeather -cinput !outputName!.joetests.txt -quadrantsize 90 -operflex 1.0 2.0 4.0 6 8 10 12 14 16 18 20 22 24 -fixnodes 1 -demandshift 1 -demanddrop 4 -dthresh 0.8 -concat
 					)
 				)
 			)		
